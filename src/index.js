029b7f36e9c1d26859a94cb528281d0120a0b3b8
Raymond <raymond96521>
2020-11-03 16:50:07 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+require("./models/User");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
+const requireAuth = require("./middlewares/requireAuth");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+// Connect to the mongodb server
+const mongoUri =
+  "mongodb+srv://admin:passwordpassword@cluster0.hzrai.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+mongoose.connection.on("connected", () => {
+  console.log("Connect to mongo instance");
+});
+mongoose.connection.on("error", (error) => {
+  console.error("Error connecting to mongo", error);
+});
+
+// Route to root file
+app.get("/", requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
+});
+
+// Listen to port 3000
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
